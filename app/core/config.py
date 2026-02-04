d778b84dcc5caa27ce77ca82709f3b59281808e5
Anand Manas <anand@example.com>
2026-02-04 21:23:41 +0530

    changes in configfile

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,10 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 def get_settings():
     return {"app_name": "Job Tracker API"}
-
-import os
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 DATABASE_URL = os.getenv("DATABASE_URL")
