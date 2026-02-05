bb5c5ad581729bbca35c46f6172d7e762c799ca1
Anand-Manas <114278575+Anand-Manas@users.noreply.github.com>
2026-02-05 19:39:30 +0530

    Update jwt.py

--- app/core/jwt.py
+++ app/core/jwt.py
@@ -7,7 +7,7 @@ from app.core.config import SECRET_KEY, ALGORITHM
 SECRET_KEY = "supersecret"
 ALGORITHM = "HS256"
 
-def create_access_token(data: dict, expires_minutes=30):
+def create_access_token(data: dict, expires_minutes=15):
     to_encode = data.copy()
     expire = datetime.now(timezone.utc) + timedelta(minutes=expires_minutes)
     to_encode.update({"exp": expire})
