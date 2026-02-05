af5aef54603db416f092676049c13f95a22fab2b
Anand Manas <anand@example.com>
2026-02-05 06:07:12 +0530

    Prepare for Render deployment

--- app/core/database.py
+++ app/core/database.py
@@ -1,7 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:Pgsql_12345@localhost:5432/postgres"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+#DATABASE_URL = "postgresql://postgres:Pgsql_12345@localhost:5432/postgres"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
